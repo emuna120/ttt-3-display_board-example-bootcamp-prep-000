5df5e5599cb6d03f312e04d43a0de30cbc232030
Amy Man <jwb74fko3ovz@beconfidential.com>
2019-02-04 01:10:10 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
   line_for_input.each {|ele| print ele }
-  puts
+ 
   puts line_for_seperation
   line_for_input.each {|ele| print ele}
   puts
